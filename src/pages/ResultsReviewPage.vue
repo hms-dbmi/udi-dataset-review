e6faa83657c32ae7932ef9dd3a28e8bd83309cb7
dev-lan <devinscottlange@gmail.com>
2025-05-05 13:58:39 -0600

    add very basic review page

--- src/pages/ResultsReviewPage.vue
+++ src/pages/ResultsReviewPage.vue
@@ -9,7 +9,20 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div>{{ reviews }}</div>
+  <div v-if="reviews.length > 0">
+    <q-card v-for="review in reviews" :key="review.data_id" class="q-mb-md">
+      <q-card-section>
+        <div><strong>Query:</strong> {{ review.query }}</div>
+        <div><strong>Review Status:</strong> {{ review.review_status }}</div>
+        <div>
+          <strong>Review Comments:</strong> {{ review.review_comments }}
+        </div>
+        <div>
+          <strong>Review Categories:</strong> {{ review.review_categories }}
+        </div>
+      </q-card-section>
+    </q-card>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
