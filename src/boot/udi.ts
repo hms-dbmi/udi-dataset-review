325ebe9843c3e2dee49c82797abed8506434f8ac
dev-lan <devinscottlange@gmail.com>
2025-05-10 07:49:34 -0600

    import toolkit styles

--- src/boot/udi.ts
+++ src/boot/udi.ts
@@ -1,5 +1,6 @@
 import { boot } from 'quasar/wrappers';
 import { UDIToolkit } from 'udi-toolkit';
+import 'udi-toolkit/dist/index.css';
 
 export default boot(({ app }) => {
   app.use(UDIToolkit);
